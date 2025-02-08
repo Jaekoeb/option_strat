bf9417101946cf202cf29ceb293787e128ad0e2e
jpertl <pertl.jakob@gmail.com>
2025-02-08 17:31:15 +0100

    Initial commit

---
+++ code/cleaning.R
@@ -0,0 +1,91 @@
+
+
+# Libraries ---------------------------------------------------------------
+
+library(data.table)
+
+# Parameters --------------------------------------------------------------
+
+
+# Set your parameters and file names
+chunk_size <- 10000
+file_name  <- "data/option.csv"
+output_file <- "data/cleaned_data.csv"
+
+# Read the header to get the column names.
+# fread() with nrows = 0 returns a data.table with the proper column names.
+header <- c("V1", "secid", "date", "symbol", "exdate", "cp_flag", "strike_price", "best_bid", "best_offer", "volume", "open_interest", "impl_volatility", "delta", "gamma", "vega", "theta", "optionid", "ss_flag", "ticker", "sic", "index_flag", "industry_group", "issuer", "exercise_style")
+# header <- names(fread(file_name, nrows = 0))
+
+# Read the number of rows
+num_rows <- 397259589
+# num_rows <- fread(file_name, select = NULL)[, .N] + 1
+
+
+# Initialize the starting line.
+# We set skip = 1 so that we skip the header line when reading chunks.
+skip <- 1
+
+# Optionally, remove any pre-existing output file.
+if (file.exists(output_file)) file.remove(output_file)
+
+
+
+## Cleaning Parameters -----------------------------------------------------
+
+# Days left until expiration
+# should be between this interval
+maturity <- c(30, 60)
+
+
+
+# Chunk Filtering ---------------------------------------------------------
+
+repeat {
+  
+  # Read a chunk from the file.
+  # We use header = FALSE because we already grabbed the header.
+  chunk <- fread(file_name, skip = skip, nrows = chunk_size, header = FALSE, mmap = FALSE)
+  
+  # Break out of the loop if no rows were read.
+  if (nrow(chunk) == 0) break
+  
+  # Assign the header names to the data.table.
+  setnames(chunk, header)
+  
+  # Convert the 'date' and 'exdate' columns to Date.
+  chunk[, date := as.Date(date)]
+  chunk[, exdate := as.Date(exdate)]
+  
+  # Compute Mid and Spread
+  chunk[, mid := (best_offer + best_bid)/2]
+  chunk[, spread := best_offer - best_bid]
+  
+  # Compute the difference in days between 'exdate' and 'date'.
+  # Coerce to numeric for easy filtering.
+  chunk[, diff := as.numeric(exdate - date)]
+  
+
+  # Filter out observations
+  chunk <- chunk[
+    diff <= maturity[2]
+    & diff >= maturity[1]
+    & best_bid > 0
+    & spread > 0
+    & open_interest > 0
+    & !is.na(mid)
+    & !is.na(spread)
+    & mday(date) < 5
+  ]
+  
+  # Write the cleaned chunk to disk.
+  # For the first chunk, create a new file; for subsequent chunks, append.
+  fwrite(chunk, output_file, append = TRUE)
+  
+  # Increase the skip counter by the number of rows read per chunk.
+  skip <- skip + chunk_size
+  
+  # Stop if skip is larger than total number of lines
+  if (skip > num_rows) {break}
+}
+
